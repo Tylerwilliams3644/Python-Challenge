c5044abb3df6acb8eb9c5530dfb5e7d8bd534b8f
Tyler Williams <tyler.williams3644@gmail.com>
2023-11-03 15:28:52 -0600

    n

--- main.py
+++ main.py
@@ -45,4 +45,7 @@ with open("budget_data.csv", "r") as csv_file:
     print(
         f"Average Change: ${round(((list_profit_loss[len(list_profit_loss) - 1]) - (list_profit_loss[0]))/ (len(list_profit_loss) - 1), 2)}"
     )
-    print(f"Greatest Increase in Profits: ", ({list_sum_profit_loss}))
+    print(f"Greatest Increase in Profits: ", {len(list_sum_profit_loss)})
+    print(
+        "Greatest Decrease in Profits: ",
+    )
